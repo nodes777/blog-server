842ff3f9bfd41eb446743c55f2c80f542e17bc40
Taylor <taylor.nodell@gmail.com>
2018-12-28 08:50:39 -0500

    adds logout route for passport

--- server/routes/auth/blogAuth.js
+++ server/routes/auth/blogAuth.js
@@ -5,29 +5,31 @@ const bcrypt = require("bcrypt");
 /* GET Google Authentication API, from /auth */
 router.get(
 	"/",
-
 	passport.authenticate("google", {
 		// scope tells what info we want from google
 		scope: ["profile"]
 	})
 );
 
+//localhost:8000/auth/logout
+router.get("/logout", (req,res) => {
+	req.logout();
+	res.redirect(`http://localhost:8080/articles`);
+});
+
 //localhost:8000/auth/auth/google/redirect?code=***
 
-// passport.authenticate fires (in passport.js) before this next function
+// passport.authenticate fires (in passport.js) before this function
 router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
 
 		//set the session
 		req.session.user = req.user._id.toString();
 		console.log("req.user._id" + req.user._id.toString())
-		//console.log("hash" + hash)
+
 		// send to a logged in URL? then clear clientside, this would jsut do client logged in conditional rendering?
 		res.redirect(`http://localhost:8080/articles/loggedIn`);
 	
-
-	// HOW TO REDIRECT TO LOCALHOST:8080 DYNAMICALLY WITH req.user???
-
-	//res.redirect("../../../articles");
+		// HOW TO REDIRECT TO LOCALHOST:8080 DYNAMICALLY WITH req.user???
 });
 
 module.exports = router;
